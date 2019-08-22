41d446a4bab6528c9932fae2bb5d2768c9e89af5
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-08-22 15:14:14 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/d/TttApplication.java
@@ -0,0 +1,12 @@
+package d;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(TttApplication.class, args);
+	}
+
+}
